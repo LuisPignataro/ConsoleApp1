68ec1b58fdd7cbf07f898314d2c44666f81f851e
Soporte UTNBA <soporte@frba.utn.edu.ar>
2019-09-30 21:10:34 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Debug.WriteLine(i);
+                if (i == 2){
+                    i+=5;
+                }
+                i++;
+            } while (i < 1000);
+        }
+    }
+}
